5c616ead0572034f623ee328bcb4364b5faa64a4
moussa1020 <moussasacko418@gmail.com>
2025-01-01 23:52:51 +0100

    first commit

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,83 @@
+"use client"
+
+import Cardwraper from "@/components/auth/card-wraper"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import z from "zod"
+import { LoginSchemas } from "@/schemas"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Formerror } from "@/components/auth/form-error"
+import { Formesucces } from "@/components/auth/form-succes"
+import { useState, useTransition } from "react"
+import { useSearchParams } from "next/navigation"
+
+export const Loginform = () => {
+
+  const searchParams = useSearchParams()
+  const urlError = searchParams.get("error") === "OAuthAccountNotLinked" ? "Email already is with different provider" : ""
+  const [error, setError] = useState<string | undefined>("")
+  const [isPanding, startTransition] = useTransition()
+
+  const form = useForm<z.infer<typeof LoginSchemas>>({
+    resolver: zodResolver(LoginSchemas),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
+
+
+  return (
+    <Cardwraper
+      headerLabel="Welcom back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+      <form className="space-y-5">
+      <div className="space-y-3">
+      <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="moussacd@gmail.com" {...field} type="text"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="******" {...field} type="password"/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <Formerror message={error || urlError} />
+      <Button type="submit" className="w-full">Login</Button>
+      </form>
+    </Form>
+    </Cardwraper>
+  )
+}
+
---
+++ components/invoice/company-header.tsx
@@ -0,0 +1,25 @@
+import { FaUpload } from "react-icons/fa";
+
+export const CompanyHeader = () => {
+  return (
+    <div className="flex gap-2 items-center">
+      <div className="">
+        <input type="file" placeholder="."/>
+      </div>
+      <div>
+      <div>
+        <h1 className="text-2xl">FACTURE</h1>
+      </div>
+      </div>
+      <div className=" min-w-52 p-2 border-2 border-black h-full rounded-sm">
+        <h2>Denomination social Entreprise </h2>
+        <h2>Adresse</h2>
+        <h2>Code Postale & Ville</h2>
+        <h2>Email</h2>
+        <h2>Tel : 01 00 00 00 00</h2>
+      </div>
+    </div>
+  )
+}
+
+
