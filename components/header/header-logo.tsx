25aed88f7e691b504d8713eab4e3416c2c8814b3
moussa1020 <moussasacko418@gmail.com>
2025-01-02 00:09:30 +0100

    image components

--- components/header/header-logo.tsx
+++ components/header/header-logo.tsx
@@ -3,7 +3,7 @@ import { IoLogoDribbble } from "react-icons/io";
 export const HeaderLogo = () => {
   return (
     <div className="flex gap-3 items-center w-1/5 text-4xl">
-      <img src="./logo_LB1.jpg" className="w-16 h-16 rounded-full"  alt="" />
+      <img src="/logo_LB1.jpg" className="w-16 h-16 rounded-full"  alt="" />
       <p>TIMES</p>
     </div>
   )
